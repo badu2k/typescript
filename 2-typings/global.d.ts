08aa34479b2fd5a3cce849d1fe7748d6d124b2c7
Vadim bikmukhametov <streetracer.56rus@gmail.com>
2026-09-09 17:52:24 +0500

    Исправление №3

---
+++ 2-typings/global.d.ts
@@ -0,0 +1,14 @@
+declare module './makeOrdinal' {
+    function makeOrdinal(word: string): string;
+    export default makeOrdinal;
+}
+
+declare module './isFinite' {
+    function isFinite(num: number): boolean;
+    export default isFinite;
+}
+
+declare module './isSafeNumber' {
+    function isSafeNumber(num: number): boolean;
+    export default isSafeNumber;
+}
